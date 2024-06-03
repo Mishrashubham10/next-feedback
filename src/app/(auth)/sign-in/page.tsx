32ad45b1183d8a9b5ae2ea669ba6ba25616919dd
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-06-03 16:20:15 +0530

    Working on next-auth

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,15 +1,36 @@
 'use client';
 
-import { signIn } from 'next-auth/react'
-import React from 'react'
+import { useSession, signIn, signOut } from 'next-auth/react';
+import React from 'react';
 
-const page = () => {
+const SignInPage = () => {
+  const { data: session } = useSession()
+
+  if (session) {
+    return (
+      <>
+      Signed in as { session.user?.email } <br />
+      <button
+        className="bg-orange-500 px-3 py-1 m-4 rounded"
+        onClick={() => signOut()}
+      >
+        Sign out
+      </button>
+      </>
+    )
+  }
+
   return (
     <>
-    Not signed in <br />
-    <button className='bg-orange-500 px-3 py-1 m-4 rounded' onClick={()=>signIn()}>Sign in</button>
+      Not signed in <br />
+      <button
+        className="bg-orange-500 px-3 py-1 m-4 rounded"
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default SignInPage;
