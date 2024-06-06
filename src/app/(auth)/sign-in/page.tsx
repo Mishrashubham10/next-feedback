d4e9f2bd200e1ade44b2fa8acd7731f8b8298d50
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-06-06 10:18:39 +0530

    Add signup page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
@@ -17,16 +15,11 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/components/ui/use-toast';
-import axios, { AxiosError } from 'axios';
-import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { singUpSchema } from '@/schemas/signUpSchema';
 import { singInSchema } from '@/schemas/signInSchema';
 import { signIn } from 'next-auth/react';
 
 export default function SignInForm() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
   const router = useRouter();
   const { toast } = useToast();
 
@@ -34,7 +27,7 @@ export default function SignInForm() {
   const form = useForm<z.infer<typeof singInSchema>>({
     resolver: zodResolver(singInSchema),
     defaultValues: {
-      email: '',
+      identifier: '',
       password: '',
     },
   });
@@ -43,19 +36,25 @@ export default function SignInForm() {
     const res = await signIn('credentials', {
       redirect: false,
       identifier: data.identifier,
-      password: data.password
-    })
+      password: data.password,
+    });
 
     if (res?.error) {
       toast({
         title: 'Login failed',
-        description: 'Invalid credentials',
-        variant: "destructive"
-      })
-    } 
+        description: 'Incorrect email or password!',
+        variant: 'destructive',
+      });
+    } else {
+      toast({
+        title: 'Error',
+        description: res?.error,
+        variant: 'destructive',
+      });
+    }
 
     if (res?.url) {
-      router.replace('/dashboard')
+      router.replace('/dashboard');
     }
   };
 
@@ -64,9 +63,9 @@ export default function SignInForm() {
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Join True Feedback
+            Join Mystery Message
           </h1>
-          <p className="mb-4">Sign up to start your anonymous adventure</p>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
         </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -75,11 +74,8 @@ export default function SignInForm() {
               control={form.control}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <Input {...field} name="email" />
-                  <p className="text-muted text-gray-400 text-sm">
-                    We will send you a verification code
-                  </p>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} name="email/username" />
                   <FormMessage />
                 </FormItem>
               )}
@@ -96,15 +92,8 @@ export default function SignInForm() {
                 </FormItem>
               )}
             />
-            <Button type="submit" className="w-full" disabled={isSubmitting}>
-              {isSubmitting ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Please wait
-                </>
-              ) : (
-                'Sign Up'
-              )}
+            <Button type="submit" className="w-full">
+              Sign in
             </Button>
           </form>
         </Form>
